Fix betti loss gradient

nn/loss/betti.py
@@ -1,33 +1,31 @@
 import torch
 import torch.nn.functional as F
+import torch.nn as nn
 import numpy as np
 from .vr import VietorisRips
 from functional.homology import drop_inf
 
 
-class BettiLoss(torch.autograd.Function):
+class BettiLossFunction(torch.autograd.Function):
     @staticmethod
-    def forward(ctx, X: torch.Tensor, Y: torch.Tensor):
-        dgms_tensorXb, dgms_tensorXd = VietorisRips(F.normalize(X))
-        dgms_tensorYb, dgms_tensorYd = VietorisRips(F.normalize(Y))
-
+    def forward(ctx, dgms_tensorXb: torch.Tensor, dgms_tensorXd: torch.Tensor, dgms_tensorYb: torch.Tensor, dgms_tensorYd: torch.Tensor):
         bY = np.array([np.array([x.shape[0] for x in drop_inf(list(dgms_tensorYb[b].unsqueeze(2).numpy(force=True)))]) for b in range(dgms_tensorYb.shape[0])])
         dgms_tensorXb, dgms_tensorXd = torch.nan_to_num(dgms_tensorXb), torch.nan_to_num(dgms_tensorXd)
 
-        ctx.save_for_backward(dgms_tensorXb, dgms_tensorXd, dgms_tensorYb, dgms_tensorYd, bY)
-        l1 = torch.tensor(
+        ctx.save_for_backward(dgms_tensorXb, dgms_tensorXd, dgms_tensorYb, dgms_tensorYd, torch.tensor(bY))
+        l1 = torch.stack(
             [
-                torch.tensor([
+                torch.stack([
                     torch.sum(1 - torch.square(dgms_tensorXd[b, dim, :bY[b, dim]] - dgms_tensorXb[b, dim, :bY[b, dim]])) for dim in range(dgms_tensorXb.shape[1])
-                ]).sum() for b in range(X.shape[0])
+                ]).sum() for b in range(bY.shape[0])
             ]
         )
-        l2 = torch.tensor(
+        l2 = torch.stack(
             [
-                torch.tensor([
+                torch.stack([
                     torch.sum(torch.square(dgms_tensorXd[b, dim, bY[b, dim]:] - dgms_tensorXb[b, dim, bY[b, dim]:]))
                     for dim in range(dgms_tensorXb.shape[1])
-                ]).sum() for b in range(X.shape[0])
+                ]).sum() for b in range(bY.shape[0])
             ]
         )
 
@@ -35,12 +33,35 @@ class BettiLoss(torch.autograd.Function):
 
     @staticmethod
     def backward(ctx, grad_output):
-        dX, dY, bY = ctx.saved_tensors
-        return torch.stack([
-            torch.stack(
-                [2 - 2 * (dX[i][:bY[i], 1] - dX[i][:bY[i], 0]) for i in range(len(dX))]
-            ),
-            torch.stack(
-                [2 * (dX[i][bY[i]:, 1] - dX[i][bY[i]:, 0]) for i in range(len(dX))]
-            )
-        ]) * grad_output
+        dgms_tensorXb, dgms_tensorXd, dgms_tensorYb, dgms_tensorYd, bY = ctx.saved_tensors
+        return (
+            torch.stack([
+                torch.stack([
+                    torch.cat([
+                        2 * (dgms_tensorXd[b, dim, :bY[b, dim]] - dgms_tensorXb[b, dim, :bY[b, dim]]),
+                        -2 * (dgms_tensorXd[b, dim, bY[b, dim]:] - dgms_tensorXb[b, dim, bY[b, dim]:])
+                    ]) for dim in range(bY.shape[1])
+                ]) for b in range(bY.shape[0])
+            ]) * grad_output,
+            torch.stack([
+                torch.stack([
+                    torch.cat([
+                        -2 * (dgms_tensorXd[b, dim, :bY[b, dim]] - dgms_tensorXb[b, dim, :bY[b, dim]]),
+                        2 * (dgms_tensorXd[b, dim, bY[b, dim]:] - dgms_tensorXb[b, dim, bY[b, dim]:])
+                    ]) for dim in range(bY.shape[1])
+                ]) for b in range(bY.shape[0])
+            ]) * grad_output,
+            None, None
+        )
+
+
+class BettiLoss(nn.Module):
+    def __init__(self, dim: int = 1):
+        super().__init__()
+        self.maxdim = dim
+        self.filtration = VietorisRips.apply
+        self.loss_fn = BettiLossFunction.apply
+
+    def forward(self, X: torch.Tensor, Y: torch.Tensor):
+        dgms_tensorXb, dgms_tensorXd, dgms_tensorYb, dgms_tensorYd = *self.filtration(F.normalize(X)), *self.filtration(F.normalize(Y))
+        return self.loss_fn(dgms_tensorXb, dgms_tensorXd, dgms_tensorYb, dgms_tensorYd)

nn/loss/vr.py
@@ -10,10 +10,9 @@ class VietorisRips(torch.autograd.Function):
         # Return birth and death times separately
         ret = [diagrams(X[b].numpy(force=True), gens=True) for b in range(X.shape[0])]
         dgms, gens = [drop_inf(r[0]) for r in ret], [[r[1][0], r[1][1][0]] for r in ret]
-        dgms_tensor, gens_tensor = diagrams_to_tensor(dgms), diagrams_to_tensor(gens, fill_value=-1)
+        dgms_tensor, gens_tensor = diagrams_to_tensor(dgms, requires_grad=True), diagrams_to_tensor(gens, fill_value=-1)
 
-        gens_birth, gens_death = gens_tensor[:, :, :-2], gens_tensor[:, :, -2:]
-        gens_birth.requires_grad = gens_death.requires_grad = False
+        gens_birth, gens_death = gens_tensor[:, :, :, :-2], gens_tensor[:, :, :, -2:]
         ctx.save_for_backward(torch.tensor(X.shape), gens_birth, gens_death)
 
         return dgms_tensor[:, :, :, 0], dgms_tensor[:, :, :, 1]
@@ -23,7 +22,10 @@ class VietorisRips(torch.autograd.Function):
         shape, gens_birth, gens_death = ctx.saved_tensors
         gens_birth, gens_death = gens_birth.numpy(force=True), gens_death.numpy(force=True)
         x = np.arange(shape[1])
-        Jb = np.logical_or.reduce((x.reshape(1, -1) == gens_birth.reshape(*gens_birth.shape, 1)), axis=3).astype(int)
-        Jd = np.logical_or.reduce((x.reshape(1, -1) == gens_death.reshape(*gens_death.shape, 1)), axis=3).astype(int)
+        Jb = np.logical_or.reduce(gens_birth.reshape(*gens_birth.shape, 1) == x.reshape(1, -1), axis=3).astype(int)
+        Jd = np.logical_or.reduce(gens_death.reshape(*gens_death.shape, 1) == x.reshape(1, -1), axis=3).astype(int)
 
-        return torch.tensor(Jb, requires_grad=False) * grad_output_birth, torch.tensor(Jd, requires_grad=False) * grad_output_death
+        return torch.sum(
+            torch.tensor(Jb) * grad_output_birth.unsqueeze(-1) + torch.tensor(Jd) * grad_output_death.unsqueeze(-1),
+            dim=1
+        ).sum(dim=1).unsqueeze(-1).expand(-1, -1, shape[-1])

utils/math.py
@@ -30,7 +30,7 @@ def inf_mask(arr: np.ndarray):
     return np.ma.fix_invalid(arr).mask
 
 
-def diagrams_to_tensor(dgms: [list[np.ndarray], list[list[np.ndarray]]], fill_value=np.nan) -> torch.Tensor:
+def diagrams_to_tensor(dgms: [list[np.ndarray], list[list[np.ndarray]]], fill_value=np.nan, requires_grad: bool = False) -> torch.Tensor:
     if isinstance(dgms[0], list):
         m_dgm = max((d for b in dgms for d in b), key=lambda x: x.shape[0]).shape[0]
         m_dgm1 = max((d for b in dgms for d in b), key=lambda x: x.shape[1]).shape[1]
@@ -42,7 +42,7 @@ def diagrams_to_tensor(dgms: [list[np.ndarray], list[list[np.ndarray]]], fill_va
                     ]
                 ) for b in range(len(dgms))
             ]
-        ), requires_grad=True)
+        ), requires_grad=requires_grad)
 
     m_dgm = max(dgms, key=lambda x: x.shape[0]).shape[0]
     m_dgm1 = max(dgms, key=lambda x: x.shape[1]).shape[1]
@@ -51,5 +51,5 @@ def diagrams_to_tensor(dgms: [list[np.ndarray], list[list[np.ndarray]]], fill_va
             [
                 np.pad(dgms[dim], ((0, m_dgm - dgms[dim].shape[0]), (0, m_dgm1 - dgms[dim].shape[1])), constant_values=fill_value) for dim in range(len(dgms))
             ]
-        ), requires_grad=True
+        ), requires_grad=requires_grad
     )