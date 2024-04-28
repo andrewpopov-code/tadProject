Fix dimension gradient

nn/loss/dimension.py
@@ -5,35 +5,36 @@ import torch.nn as nn
 class MLE(torch.autograd.Function):
     @staticmethod
     def forward(ctx, distances: torch.Tensor):
-        _, n, k = distances.shape[-1]
-        dim_ptw = (k - 1) / torch.log(distances[:, -1].unsqueeze(1) / distances).sum(-1)
+        _, n, k = distances.shape
+        dim_ptw = (k - 1) / torch.log(distances[:, :, -1].unsqueeze(2) / distances).sum(dim=-1)
         dim = 1 / torch.mean(1 / dim_ptw, dim=-1)
-        ctx.save_for_backward(distances, n, k, dim_ptw, dim)
+        ctx.save_for_backward(distances, torch.tensor(distances.shape), dim_ptw, dim)
         return dim
 
     @staticmethod
     def backward(ctx, grad_output):
-        distances, n, k, dim_ptw, dim = ctx.saved_tensors
-        left = -1 / (distances[:, :, :-1] * n * (k - 1) * dim_ptw * dim_ptw * dim * dim)
-        right = 1 / (distances[:, :, -1] * n * dim_ptw * dim_ptw * dim * dim).unsqueeze(-1)
-        return torch.hstack((left, right)) * grad_output
+        distances, shape, dim_ptw, dim = ctx.saved_tensors
+        _, n, k = shape
+        left = -1 / (n * (k - 1)* distances[:, :, :-1] * (dim_ptw * dim_ptw * dim * dim).unsqueeze(dim=2))
+        right = 1 / (n * distances[:, :, -1] * dim_ptw * dim_ptw * dim * dim).unsqueeze(dim=2)
+        return torch.cat((left, right), dim=2) * grad_output
 
 
 class MOM(torch.autograd.Function):
     @staticmethod
     def forward(ctx, distances: torch.Tensor):
-        _, n, k = distances.shape[-1]
-        ctx.save_for_backward(distances, n, k)
+        ctx.save_for_backward(distances, torch.tensor(distances.shape))
         Tk = distances[:, :, -1]
         T = distances.mean(dim=-1)
         return (T / (Tk - T)).mean(dim=-1)
 
     @staticmethod
     def backward(ctx, grad_output):
-        distances, n, k = ctx.saved_tensors
-        left = distances[:, :, -1] / (n * k * (distances[:, :, -1] - distances.mean(dim=-1)) * (distances[:, :, -1] - distances.mean(dim=-1)))
+        distances, shape = ctx.saved_tensors
+        _, n, k = shape
+        left = distances[:, :, :-1] / (n * k * (distances[:, :, :-1] - distances.mean(dim=-1).unsqueeze(dim=2)) * (distances[:, :, :-1] - distances.mean(dim=-1).unsqueeze(dim=2)))
         right = (distances[:, :, -1] - distances.sum(dim=-1)) / (n * k * (distances[:, :, -1] - distances.mean(dim=-1)) * (distances[:, :, -1] - distances.mean(dim=-1)))
-        return torch.hstack((left, right.unsqueeze(-1))) * grad_output
+        return torch.cat((left, right.unsqueeze(-1)), dim=2) * grad_output
 
 
 class Dimension(nn.Module):
@@ -43,5 +44,5 @@ class Dimension(nn.Module):
         self.est = MLE.apply if method == 'mle' else MOM.apply
 
     def forward(self, X: torch.Tensor):
-        dist = torch.cdist(X, X).topk(k=self.k, largest=False, sorted=True).values[:, :, 1:]
+        dist = torch.cdist(X, X).topk(k=self.k + 1, largest=False, sorted=True).values[:, :, 1:]
         return self.est(dist)

nn/loss/vr.py
@@ -15,8 +15,8 @@ class VietorisRips(torch.autograd.Function):
             [
                 np.stack(
                     [
-                        np.pad(dgms[b][0], ((0, m_dgm - dgms[b][0].shape[0]), (0, 0)), constant_values=np.inf),
-                        np.pad(dgms[b][1], ((0, m_dgm - dgms[b][1].shape[0]), (0, 0)), constant_values=np.inf)
+                        np.pad(dgms[b][0], ((0, m_dgm - dgms[b][0].shape[0]), (0, 0)), constant_values=np.nan),
+                        np.pad(dgms[b][1], ((0, m_dgm - dgms[b][1].shape[0]), (0, 0)), constant_values=np.nan)
                     ]
                 ) for b in range(X.shape[0])
             ]