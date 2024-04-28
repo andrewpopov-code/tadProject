Fix VR gradients

nn/loss/betti.py
@@ -1,14 +1,14 @@
-from typing import Any
-
 import torch
 import numpy as np
-from functional.homology import diagrams
+from .vr import VietorisRips
 
 
 class BettiLoss(torch.autograd.Function):
     @staticmethod
     def forward(ctx, X: torch.Tensor, Y: torch.Tensor):
-        dX, dY = diagrams(X.numpy(force=True)), diagrams(Y.numpy(force=True))
+        dgms_tensorXb, dgms_tensorXd = VietorisRips(X)
+        dgms_tensorYb, dgms_tensorYd = VietorisRips(Y)
+
         bY = [a.shape[0] for a in dY]
         ctx.save_for_backward(dX, dY, bY)
         l1 = np.sum([np.sum(1 - (dX[i][:bY[i], 1] - dX[i][:bY[i], 0]) * (dX[i][:bY[i], 1] - dX[i][:bY[i], 0])) for i in range(len(dX))])
@@ -16,7 +16,7 @@ class BettiLoss(torch.autograd.Function):
         return l1 + l2
 
     @staticmethod
-    def backward(ctx: Any, grad_output: Any):
+    def backward(ctx, grad_output):
         dX, dY, bY = ctx.saved_tensors
         return torch.stack([
             torch.stack(

nn/loss/vr.py
@@ -1,6 +1,7 @@
 import numpy as np
 import torch
-from functional.homology import diagrams
+from functional.homology import diagrams, drop_inf
+from utils.math import diagrams_to_tensor
 
 
 class VietorisRips(torch.autograd.Function):
@@ -8,42 +9,20 @@ class VietorisRips(torch.autograd.Function):
     def forward(ctx, X: torch.Tensor):
         # Return birth and death times separately
         ret = [diagrams(X[b].numpy(force=True), gens=True) for b in range(X.shape[0])]
-        dgms, gens = [r[0] for r in ret], [(r[1][0], r[1][1][0]) for r in ret]
-        m_dgm = max(max(len(d[0]), len(d[1])) for b in dgms for d in b)
-
-        dgms_tensor = torch.tensor(np.stack(
-            [
-                np.stack(
-                    [
-                        np.pad(dgms[b][0], ((0, m_dgm - dgms[b][0].shape[0]), (0, 0)), constant_values=np.nan),
-                        np.pad(dgms[b][1], ((0, m_dgm - dgms[b][1].shape[0]), (0, 0)), constant_values=np.nan)
-                    ]
-                ) for b in range(X.shape[0])
-            ]
-        ))
-
-        gens_tensor = torch.tensor(np.stack(
-            [
-                np.stack(
-                    [
-                        np.pad(gens[b][0], ((0, m_dgm - dgms[b][0].shape[0]), (0, 1)), constant_values=-1),
-                        np.pad(gens[b][1], ((0, m_dgm - dgms[b][1].shape[0]), (0, 0)), constant_values=-1)
-                    ]
-                ) for b in range(X.shape[0])
-            ]
-        ))
+        dgms, gens = [drop_inf(r[0]) for r in ret], [[r[1][0], r[1][1][0]] for r in ret]
+        dgms_tensor, gens_tensor = diagrams_to_tensor(dgms), diagrams_to_tensor(gens, fill_value=-1)
 
         gens_birth, gens_death = gens_tensor[:, :, :-2], gens_tensor[:, :, -2:]
-        ctx.save_for_backward(X.shape, gens_birth, gens_death)
+        ctx.save_for_backward(torch.tensor(X.shape), gens_birth, gens_death)
 
-        return dgms_tensor[:, :, 0], dgms_tensor[:, :, 1]
+        return dgms_tensor[:, :, :, 0], dgms_tensor[:, :, :, 1]
 
     @staticmethod
     def backward(ctx, grad_output_birth, grad_output_death):
         shape, gens_birth, gens_death = ctx.saved_tensors
         gens_birth, gens_death = gens_birth.numpy(force=True), gens_death.numpy(force=True)
         x = np.arange(shape[1])
-        Jd = (x.reshape(-1, 1) == gens_death[:, :, 0]) | (x.reshape(-1, 1) == gens_death[:, :, 1])
-        Jb = (x.reshape(-1, 1) == gens_birth[:, :, 0]) | (x.reshape(-1, 1) == gens_birth[:, :, 1])
+        Jb = np.logical_or.reduce((x.reshape(1, -1) == gens_birth.reshape(*gens_birth.shape, 1)), axis=3).astype(int)
+        Jd = np.logical_or.reduce((x.reshape(1, -1) == gens_death.reshape(*gens_death.shape, 1)), axis=3).astype(int)
 
-        return torch.tensor(Jb) * grad_output_birth, torch.tensor(Jd) * grad_output_death
+        return torch.tensor(Jb, requires_grad=False) * grad_output_birth, torch.tensor(Jd, requires_grad=False) * grad_output_death