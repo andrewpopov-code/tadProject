61fcbdcf2264d1e22dcea47a9804376208080cbf
andrewpopov-code <agpopov_1@edu.hse.ru>
2024-04-30 14:10:25 +0300

    Fix backward context

--- nn/loss/vr.py
+++ nn/loss/vr.py
@@ -4,13 +4,11 @@ from functional.homology import diagrams, drop_inf
 from utils.math import diagrams_to_tensor
 
 
-def filtration_return(ctx, ret: list):
+def filtration_return(ret: list):
     dgms, gens = [drop_inf(r[0]) for r in ret], [[r[1][0], *r[1][1]] for r in ret]
     dgms_tensor, gens_tensor = diagrams_to_tensor(dgms, requires_grad=True), diagrams_to_tensor(gens, fill_value=-1)
 
     gens_birth, gens_death = gens_tensor[:, :, :, :-2], gens_tensor[:, :, :, -2:]
-    ctx.save_for_backward(gens_birth, gens_death)
-
     return dgms_tensor[:, :, :, 0], dgms_tensor[:, :, :, 1], gens_birth, gens_death
 
 
@@ -20,14 +18,13 @@ def boundary_matrix(gens: np.ndarray, x: np.ndarray):
 
 class VietorisRips(torch.autograd.Function):
     @staticmethod
-    def forward(ctx, X: torch.Tensor):
+    def forward(X: torch.Tensor):
         # Return birth and death times separately
-        ctx.save_for_backward(torch.tensor(X.shape))
         ret = [diagrams(X[b].numpy(force=True), gens=True) for b in range(X.shape[0])]
-        return filtration_return(ctx, ret)
+        return filtration_return(ret)
 
     @staticmethod
-    def backward(ctx, grad_output_birth, grad_output_death):
+    def backward(ctx, grad_output_birth, grad_output_death, *args):
         shape, gens_birth, gens_death = ctx.saved_tensors
         gens_birth, gens_death = gens_birth.numpy(force=True), gens_death.numpy(force=True)
         x = np.arange(shape[1])
@@ -38,10 +35,14 @@ class VietorisRips(torch.autograd.Function):
             dim=1
         ).sum(dim=1).unsqueeze(-1).expand(-1, -1, shape[-1])
 
+    @staticmethod
+    def setup_context(ctx, inputs, outputs):
+        ctx.save_for_backward(torch.tensor(inputs[0].shape), *outputs[2:])
+
 
 class CrossBarcode(torch.autograd.Function):
     @staticmethod
-    def forward(ctx, X: torch.Tensor, Y: torch.Tensor):
+    def forward(X: torch.Tensor, Y: torch.Tensor):
         XX, YY = torch.cdist(X, X), torch.cdist(Y, Y)
         inf_block = torch.triu(torch.full_like(XX, torch.inf), 1) + XX
         M = torch.cat([
@@ -49,12 +50,15 @@ class CrossBarcode(torch.autograd.Function):
             torch.cat([inf_block, torch.minimum(XX, YY), torch.full((XX.shape[0], XX.shape[1], 1), torch.inf)], dim=-1),
             torch.cat([torch.zeros((XX.shape[0], 1, XX.shape[1])), torch.full((XX.shape[0], 1, XX.shape[1]), torch.inf), torch.zeros((XX.shape[0], 1, 1))], dim=-1)
         ], dim=1)
-        ctx.save_for_backward(torch.tensor(X.shape), torch.tensor(Y.shape))
         ret = [diagrams(M[b].numpy(force=True), distances=True, gens=True) for b in range(X.shape[0])]
-        return filtration_return(ctx, ret)
+        return filtration_return(ret)
 
     @staticmethod
-    def backward(ctx, grad_output_birth, grad_output_death):
+    def setup_context(ctx, inputs, outputs):
+        ctx.save_for_backward(torch.tensor(inputs[0].shape), torch.tensor(inputs[1].shape), *outputs[2:])
+
+    @staticmethod
+    def backward(ctx, grad_output_birth, grad_output_death, *args):
         shapeX, shapeY, gens_birth, gens_death = ctx.saved_tensors
         gens_birth, gens_death = gens_birth.numpy(force=True), gens_death.numpy(force=True)
         x, y = np.arange(shapeX[1]), shapeX[1] + np.arange(shapeY[1])
