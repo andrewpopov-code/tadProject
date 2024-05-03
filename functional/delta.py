Fix cross-barcode gradient

functional/delta.py
@@ -14,7 +14,7 @@ def min_max_prod_torch(x: torch.Tensor, y: torch.Tensor) -> torch.Tensor:
 def delta_hyperbolicity(X: np.ndarray, distances: bool = True) -> float:
     p = 0
     d = X if distances else distance_matrix(X, X)
-    row, col = d[p, :].reshape(X.shape[0], 1), d[:, p].reshape(1, X.shape[0])
+    row, col = np.expand_dims(d[p, :], axis=-1), np.expand_dims(d[:, p], axis=0)
     A = 0.5 * (row + col - d)
     maxmin = min_max_prod(A, A)
 

functional/homology.py
@@ -99,7 +99,7 @@ def r_cross_barcode(X: np.array, Y: np.array, maxdim: int = 1):
     inf_block = np.triu(np.full_like(XX, np.inf), 1) + XX
 
     M = np.block([
-        [np.zeros_like(XX), inf_block.T, np.zeros((XX.shape[0], 1))],
+        [XX, inf_block.T, np.zeros((XX.shape[0], 1))],
         [inf_block, np.minimum(XX, YY), np.full((XX.shape[0], 1), np.inf)],
         [np.zeros((1, XX.shape[0])), np.full((1, XX.shape[0]), np.inf), 0]
     ])

nn/loss/dimension.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+from utils.math import neighbors
 
 
 class MLE(torch.autograd.Function):
@@ -53,5 +54,5 @@ class Dimension(nn.Module):
         self.est = TwoNN() if method == 'two_nn' else MLE.apply if method == 'mle' else MOM.apply
 
     def forward(self, X: torch.Tensor):
-        dist = torch.cdist(X, X).topk(k=self.k + 1, largest=False, sorted=True).values[:, :, 1:]
+        dist, _ = neighbors(X, self.k)
         return self.est(dist)

nn/loss/functional.py
@@ -1,6 +1,7 @@
 import torch
 from .vr import VietorisRips
 from functional.homology import drop_inf
+from utils.math import batch_select, neighbors, set_ops
 
 
 def pq_loss(dgms_tensorXb: torch.Tensor, dgms_tensorXd: torch.Tensor, left: float, right: float):
@@ -10,26 +11,17 @@ def pq_loss(dgms_tensorXb: torch.Tensor, dgms_tensorXd: torch.Tensor, left: floa
 
 
 def signature_loss(X: torch.Tensor, Z: torch.Tensor):  # FIXME
-    gens_tensorXb, gens_tensorXd = VietorisRips.apply(X)[2:]
-    gens_tensorZb, gens_tensorZd = VietorisRips.apply(Z)[2:]
+    _, gens_tensorXd = VietorisRips.apply(X)[2:]
+    _, gens_tensorZd = VietorisRips.apply(Z)[2:]
     dX, dZ = torch.cdist(X, X), torch.cdist(Z, Z)
-    Lxz = Lzx = 0
 
-    for b in range(X.shape[0]):
-        for dim in range(gens_tensorXb.shape[1]):
-            gensb, gensd = drop_inf(gens_tensorXb[b, dim, :dim + 1]), drop_inf(gens_tensorXd[b, dim])
-            distXX = dX[b, gensb, gensd]
-            distZX = dZ[b, gensb, gensd]
-            Lxz += torch.square(distXX - distZX)
+    # FIXME: Different axes select
+    distXX = batch_select(dX, gens_tensorXd)
+    distZX = batch_select(dZ, gens_tensorXd)
+    distZZ = batch_select(dZ, gens_tensorZd)
+    distXZ = batch_select(dX, gens_tensorZd)
 
-            gensb, gensd = drop_inf(gens_tensorZb[b, dim, :dim + 1]), drop_inf(gens_tensorZd[b, dim])
-            distZZ = dZ[b, gensb, gensd]
-            distXZ = dX[b, gensb, gensd]
-            Lzx += torch.square(distXZ - distZZ)
-
-    Lxz = torch.sqrt(Lxz) / 2
-    Lzx = torch.sqrt(Lzx) / 2
-    return Lxz + Lzx
+    return torch.sqrt(torch.square(distXX - distZX).sum()) / 2 + torch.sqrt(torch.square(distXZ - distZZ).sum()) / 2
 
 
 def ph_dimension_loss(X: torch.Tensor):
@@ -46,3 +38,27 @@ def ph_dimension_loss(X: torch.Tensor):
             (n.shape[0] * torch.sum(n * e, dim=1) - n.sum() * e.sum(dim=1)) / (X.shape[1] * torch.square(n).sum() - torch.square(n.sum()))
         )
     )
+
+
+def stress_loss(X: torch.Tensor, Z: torch.Tensor):
+    dX, dZ = torch.cdist(X, X), torch.cdist(Z, Z)
+    return torch.sqrt(torch.square(dX - dZ).sum() / torch.square(dZ).sum())
+
+
+def trustworthiness_loss(X: torch.Tensor, Z: torch.Tensor, k: int):
+    dX, iX = neighbors(X, k)
+    dZ, iZ = neighbors(Z, k)
+
+    onlyX = set_ops(iX, iZ)[3]
+    return onlyX.sum()
+
+
+def continuity_loss(X: torch.Tensor, Z: torch.Tensor, k: int):
+    return trustworthiness_loss(Z, X, k)
+
+
+def intersection_loss(X: torch.Tensor, Z: torch.Tensor, k: int):
+    dX, iX = neighbors(X, k)
+    dZ, iZ = neighbors(Z, k)
+
+    return 1 - torch.sum(set_ops(iX, iZ)[1] / k) / X.shape[1]

nn/loss/vr.py
@@ -2,7 +2,7 @@ import numpy as np
 import torch
 from torch.nn.functional import normalize
 from functional.homology import diagrams, drop_inf
-from utils.math import diagrams_to_tensor, gens_to_tensor, boundary_matrix
+from utils.math import diagrams_to_tensor, gens_to_tensor, boundary_matrix, batch_select
 
 
 def filtration_return(ret: list):
@@ -13,17 +13,31 @@ def filtration_return(ret: list):
     return dgms_tensor[:, :, :, 0], dgms_tensor[:, :, :, 1], gens_birth, gens_death
 
 
-def batch_select(X: torch.Tensor, gens: torch.Tensor) -> torch.Tensor:
-    dummy = gens.unsqueeze(-1).expand(*gens.shape, X.shape[-1]).squeeze(-2)
-    return torch.gather(X.detach().repeat(1, 2, 1, 1), 2, dummy)
-
-
 def grad_subroutine(X: torch.Tensor, gens: torch.Tensor) -> torch.Tensor:
     boundary1 = boundary_matrix(gens[:, :, :, :1].numpy(force=True), np.arange(X.shape[1]))
     boundary0 = boundary_matrix(gens[:, :, :, 1:].numpy(force=True), np.arange(X.shape[1]))
     norm = normalize(batch_select(X, gens[:, :, :, 1]) - batch_select(X, gens[:, :, :, 0]), dim=-1).unsqueeze(-2)
     J0 = -norm * torch.tensor(boundary0).unsqueeze(-1)
     J1 = norm * torch.tensor(boundary1).unsqueeze(-1)
+    return J0 + J1
+
+
+def cross_grad_subroutine(X: torch.Tensor, Y: torch.Tensor, gens: torch.Tensor) -> torch.Tensor:
+    Z = torch.cat((X, Y), dim=-2)
+    boundary1 = boundary_matrix(gens[:, :, :, :1].numpy(force=True), np.arange(Z.shape[1] + 1))
+    boundary0 = boundary_matrix(gens[:, :, :, 1:].numpy(force=True), np.arange(Z.shape[1] + 1))
+
+    XX = gens % X.shape[0]
+    X_normed = normalize(batch_select(Z, XX[:, :, :, 1]) - batch_select(Z, XX[:, :, :, 0]), dim=-1)
+    YY = gens % X.shape[0] + X.shape[0]
+    Y_normed = normalize(batch_select(Z, YY[:, :, :, 1]) - batch_select(Z, YY[:, :, :, 0]), dim=-1)
+    Xnorm = torch.norm(batch_select(Z, XX[:, :, :, 1]) - batch_select(Z, XX[:, :, :, 0]))
+    Ynorm = torch.norm(batch_select(Z, YY[:, :, :, 1]) - batch_select(Z, YY[:, :, :, 0]))
+    JYY = ((Xnorm <= Ynorm) * X_normed + (X_normed > Y_normed) * Y_normed) * torch.all((X.shape[1] <= gens) & (gens < 2 * X.shape[1]), dim=-1).unsqueeze(-1)
+    JXXY = X_normed * torch.all((X.shape[1] > gens[:, :, :, :1]) & (gens[:, :, :, 1:] < 2 * X.shape[1]), dim=-1).unsqueeze(-1)
+
+    J0 = -(JYY + JXXY).unsqueeze(-2) * torch.tensor(boundary0).unsqueeze(-1)
+    J1 = (JYY + JXXY).unsqueeze(-2) * torch.tensor(boundary1).unsqueeze(-1)
     return J0 + J1
 
 
@@ -52,7 +66,7 @@ class CrossBarcode(torch.autograd.Function):
         XX, YY = torch.cdist(X, X), torch.cdist(Y, Y)
         inf_block = torch.triu(torch.full_like(XX, torch.inf), 1) + XX
         M = torch.cat([
-            torch.cat([torch.zeros_like(XX), inf_block.transpose(1, 2), torch.zeros((XX.shape[0], XX.shape[1], 1))], dim=-1),
+            torch.cat([XX, inf_block.transpose(1, 2), torch.zeros((XX.shape[0], XX.shape[1], 1))], dim=-1),
             torch.cat([inf_block, torch.minimum(XX, YY), torch.full((XX.shape[0], XX.shape[1], 1), torch.inf)], dim=-1),
             torch.cat([torch.zeros((XX.shape[0], 1, XX.shape[1])), torch.full((XX.shape[0], 1, XX.shape[1]), torch.inf), torch.zeros((XX.shape[0], 1, 1))], dim=-1)
         ], dim=1)
@@ -61,23 +75,15 @@ class CrossBarcode(torch.autograd.Function):
 
     @staticmethod
     def setup_context(ctx, inputs, outputs):
-        ctx.save_for_backward(torch.tensor(inputs[0].shape), torch.tensor(inputs[1].shape), *outputs[2:])
+        ctx.save_for_backward(*outputs[2:], *inputs)
 
     @staticmethod
     def backward(ctx, grad_output_birth, grad_output_death, *args):
-        shapeX, shapeY, gens_birth, gens_death = ctx.saved_tensors
-        gens_birth, gens_death = gens_birth.numpy(force=True), gens_death.numpy(force=True)
-        x, y = np.arange(shapeX[1]), shapeX[1] + np.arange(shapeY[1])
-        JbX, JdX = boundary_matrix(gens_birth, x), boundary_matrix(gens_death, x)
-        JbY, JdY = boundary_matrix(gens_birth, y), boundary_matrix(gens_death, y)
+        gens_birth, gens_death, X, Y = ctx.saved_tensors
+        gens_birth.sort()
+        gens_death.sort()
+        Jb = cross_grad_subroutine(X, Y, gens_birth) * grad_output_birth.unsqueeze(-1).unsqueeze(-1)
+        Jd = cross_grad_subroutine(X, Y, gens_death) * grad_output_death.unsqueeze(-1).unsqueeze(-1)
 
-        return (
-            torch.sum(
-                torch.tensor(JbX) * grad_output_birth.unsqueeze(-1) + torch.tensor(JdX) * grad_output_death.unsqueeze(-1),
-                dim=1
-            ).sum(dim=1).unsqueeze(-1).expand(-1, -1, shapeX[-1]),
-            torch.sum(
-                torch.tensor(JbY) * grad_output_birth.unsqueeze(-1) + torch.tensor(JdY) * grad_output_death.unsqueeze(-1),
-                dim=1
-            ).sum(dim=1).unsqueeze(-1).expand(-1, -1, shapeY[-1])
-        )
+        J = (Jb + Jd).sum(dim=1).sum(dim=1)
+        return J[:, :X.shape[0]], J[:, X.shape[0]:-1]